5226c695fcb6b19b8d72d460c950cf7b2987c5f8
daveclinton <clintondavid46@gmail.com>
2022-11-16 21:59:45 +0300

    chukua

---
+++ src/components/testimonials.jsx
@@ -0,0 +1,35 @@
+export const Testimonials = (props) => {
+  return (
+    <div id="testimonials">
+      <div className="container">
+        <div className="section-title text-center">
+          <h2>Our Mission</h2>
+          <p>
+            {" "}
+            Promotion of gender equality and equal address of youths, young
+            women, orphans, widows and elderly persons through training them in
+            various life skills and advocating against social injustice.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  <div className="testimonial">
+                    <div className="testimonial-image">
+                      {" "}
+                      <img src={d.img} alt="" />{" "}
+                    </div>
+                    <div className="testimonial-content">
+                      <p>"{d.text}"</p>
+                      <div className="testimonial-meta"> - {d.name} </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
